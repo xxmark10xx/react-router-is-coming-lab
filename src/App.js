776970897869fdeed577fe791ce1721c20455af5
weston-bailey <wdbailey87@gmail.com>
2021-06-27 14:04:23 -0700

    stub routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Houses from './components/Houses'
 import House from './components/House'
 import Member from './components/Member'
@@ -9,11 +15,26 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Houses />
+        <Router>
+          <Route 
+            exact path="/"
+            component={Houses} 
+          />
 
-        <House />
+          <Route 
+            exact path="/houses/:id" 
+            render={() => {
+              return <House />
+            }} 
+          />
 
-        <Member />
+          <Route 
+            path="/houses/:id/member/:id" 
+            render={() => {
+              return <Member />
+            }}
+          />
+        </Router>
       </div>
     )
   }
