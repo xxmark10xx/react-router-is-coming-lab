ab005c3ba9da6c396e0391bc2c8c0ea3a5434059
weston-bailey <wdbailey87@gmail.com>
2021-06-27 14:08:45 -0700

    import gameOfThrones data

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
-  Link
 } from 'react-router-dom';
 
 import Houses from './components/Houses'
 import House from './components/House'
 import Member from './components/Member'
 
+import gameOfThrones from './gameOfThrones'
 
 class App extends React.Component {
   render() {
+    const data = gameOfThrones
+    console.log(data)
+
     return (
       <div>
         <Router>
