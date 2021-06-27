a9ac8c1f4dab8548375e708d6a58a89a34f373b1
weston-bailey <wdbailey87@gmail.com>
2021-06-27 14:44:03 -0700

    update House.jsx

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,11 +1,31 @@
-export default function House() {
+import { Link } from "react-router-dom"
+
+export default function House(props) {
+  console.log(props)
+  const members = props.people.map(person => {
+    return (
+      <li key={`${person.name}-${person.id}`}>
+        <Link to={`${props.match.url}/member/${person.id}`}>{person.name}</Link>
+      </li>
+    )
+  })
+
   return (
     <div className="page">
         <div className="header">
-            <h2>Members of a GoT House</h2>
+            <h2>House of {props.name}</h2>
+
+            {/* bonus: */}
+            <a
+              href={`https://awoiaf.westeros.org/index.php/${props.wikiSuffix}`}
+              target="_blank"
+            >
+              more info
+            </a>
+
         </div>
         <ul className="list">
-            A list of members
+            {members}
         </ul>
     </div>
   )
